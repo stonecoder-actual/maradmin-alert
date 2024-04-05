679d629507f81adeedc09138a2a7310de66555b6
stonecoder-actaul <samstnstrt@mailfence.com>
2024-04-05 14:02:37 -0400

    update

--- maradminNameGrabber.py
+++ maradminNameGrabber.py
@@ -6,43 +6,28 @@ import re
 
 url = "https://www.marines.mil/News/Messages/Messages-Display/Article/3707403/fy24-approved-selections-to-gunnery-sergeant/"
 response = requests.get(url)
-print(response)
 
 soup = BeautifulSoup(response.content, "html.parser")
 bodyText = (soup.find('div', class_='body-text'))
 
 if bodyText:
-            # Extract text content from the <div> element
-            text_content = bodyText.get_text(separator='\r\n')
+        # Extract text content from the <div> element
+        text_content = bodyText.get_text(separator='\r\n')
 
-            # Split the text content into lines
-            lines = text_content.split('\r\n')
-            print(len(lines[41]))
+        # Split the text content into lines
+        lines = text_content.split('\\r\\n')
+        print(lines[40:41])
+        pattern = r'([A-Z]+\s+[A-Z]{2})'
 
-            namesInMaradmins = []
+        namesInMaradmins = []
 
-            for line in lines:
-                    namesInMaradmins.append(re.sub('[A-Z]+\s+[A-Z]{2}', '', line))
+        for line in lines:
+                matches = re.findall(pattern,line)
+                if len(matches) > 0:
+                        namesInMaradmins.append(matches)
+        
+        for place,line in enumerate(namesInMaradmins):
+                line = str(line)
+                namesInMaradmins[place] = line.strip('\\xa0')
 
-            print(namesInMaradmins)
-            
-            
-
-            # for line in lines:
-            #         print(line)
-
-            # names_from_page = []
-            # for line in lines:
-            #     # Extract information from each line
-            #     parts = line.split()
-            #     if len(parts) >= 5:
-            #         last_name = parts[0]
-            #         print(last_name)
-            #         # middle_initial = parts[1]
-            #         first_name = parts[2]
-            #         dor = parts[3]
-            #         mcc = parts[4]
-
-            #         names_from_page.append((first_name.lower(), last_name.lower(), "1STLT"))
-            
-            # print(names_from_page)
+print(namesInMaradmins[1]) 
