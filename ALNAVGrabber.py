847c921adf7199e8f382b62497b2881d356a6ff3
stonecoder-actual <154912163+stonecoder-actual@users.noreply.github.com>
2025-02-05 20:36:42 -0500

    Create ALNAVGrabber.py
    add

---
+++ ALNAVGrabber.py
@@ -0,0 +1,29 @@
+''' IMPORT STANDARD LIBRARIES '''
+import feedparser
+import requests
+import csv
+import logging
+
+
+''' IMPORT 3RD PARTY LIBRARIES '''
+from bs4 import BeautifulSoup
+
+''' DEFINE PSEUDO CONSTANTS '''
+alnav25 = "https://www.mynavyhr.navy.mil/Portals/55/Messages/ALNAV/ALN2025/ALN25"
+
+''' LOCAL FUNCTIONS '''
+
+
+''' LOCAL CLASSES '''
+# NONE
+
+''' MAIN ENTRY POINT '''
+
+if __name__ == "__main__":
+    ''' Main Script Entry Point '''
+    try:
+    
+    
+    except Exception as err:
+        
+        logging.critical("\n\nScript Aborted     ", "Exception =     ", err) 
